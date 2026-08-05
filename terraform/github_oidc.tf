548d79f58464df2045926d3744e0fc1d56b9e71c
shaurya-security <amank.47659@gmail.com>
2026-08-05 12:50:48 +0530

    fix: update GitHub OIDC IAM trust policy to support wildcard repo subs

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -51,12 +51,12 @@ data "aws_iam_policy_document" "github_oidc_assume_role" {
 
     condition {
 
-      test = "StringEquals"
+      test = "StringLike"
 
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_owner}/${var.github_repo}:ref:refs/heads/main"
+        "repo:${var.github_owner}/${var.github_repo}:*"
       ]
     }
   }
