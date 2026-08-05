117a2206557ea7bda0b0abdf67694f7464267c51
shaurya-security <amank.47659@gmail.com>
2026-08-05 22:49:25 +0530

    fix: failure was due rename

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = "flask-ecs-devops-pipeline"
   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
